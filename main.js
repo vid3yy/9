541468a32cd7a19e73ea477489233e73e83db654
vid3yy <011jajuli@gmail.com>
2026-06-14 14:51:13 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function joinWhatsAppGroup() {
 }
 
 function openFacebookPage() {
-    window.open("https://www.facebook.com/profile.php?id=61586069892795");
+    window.open("https://www.facebook.com/share/g/1EQyiJiHYV/");
 }
 const video = document.getElementById("video");
 const overlay = document.getElementById("videoOverlay");
@@ -32,7 +32,7 @@ overlay.addEventListener("click", () => {
     overlay.classList.remove("show");
 
     // Aksi setelah klik
-    window.open("https://s.shopee.co.id/W2V7VMsXJ", "_blank");
+    window.open("https://s.shopee.co.id/2g8lhXuWik", "_blank");
 });
 
 
